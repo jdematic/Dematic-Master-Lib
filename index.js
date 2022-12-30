a9ac5f58392ad8da22102ece07a17286fd2d4dd5
james.lyons <james.lyons@dematic.com>
2022-12-30 21:44:14 +0000

    added version info to index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Set of libraries functions to be used in projects
 //Created by: JWL
 //Date: 2022-12-30
-//Last modified: 2022/12/30 21:38:30
+//Last modified: 2022/12/30 21:44:02
 //Version: 0.0.1
 
 //import the date time functions
@@ -18,4 +18,6 @@ module.exports = {
   dateTime: dateTime,
   stringManipulation: stringManipulation,
   objectTest: objectTest,
+
+  version: "0.0.1",
 };
