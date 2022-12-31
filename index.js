477b11687b36f8ac4be551a7e3b70d2afd27ca8a
james.lyons <james.lyons@dematic.com>
2022-12-31 00:05:24 +0000

    mysql index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Set of libraries functions to be used in projects
 //Created by: JWL
 //Date: 2022-12-30
-//Last modified: 2022/12/30 21:49:24
+//Last modified: 2022/12/31 00:05:04
 //Version: 0.0.1
 
 //import the date time functions
@@ -13,11 +13,15 @@ const stringManipulation = require("./StringManipulation");
 //import the object manipulation functions
 const objectTest = require("./objectTest");
 
+//import the mysql functions
+const mysql = require("./mysql");
+
 //export functions
 module.exports = {
   dateTime: dateTime,
   stringManipulation: stringManipulation,
   objectTest: objectTest,
+  mysql: mysql,
 
   version: "0.0.1",
 };
