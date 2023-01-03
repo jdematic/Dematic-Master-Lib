7eba14a7a124cc84c33f264c1719957629d4d468
james.lyons <james.lyons@dematic.com>
2023-01-03 15:34:50 +0000

    first console

---
+++ console.js
@@ -0,0 +1,27 @@
+//Set of console functions to be used in projects
+//Created by: JWL
+//Date: 2022-12-30
+//Last modified: 2023/01/03 15:32:32
+//Version: 0.0.1
+
+//log out a message to the console with a timestamp always
+function log(message) {
+  var date = new Date();
+  var time = date.toLocaleTimeString();
+  console.log(time + " " + message);
+}
+
+//log out a message to the console with a timestamp only if the debug flag is set
+function logDebug(message) {
+  if (global.debugFlag) {
+    var date = new Date();
+    var time = date.toLocaleTimeString();
+    console.log(time + " " + message);
+  }
+}
+
+//export the functions
+module.exports = {
+  logAlways: logAlways,
+  logDebug: logDebug,
+};
