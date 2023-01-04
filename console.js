4906b324884e21a7317f6919cce531753c70c0ec
james.lyons <james.lyons@dematic.com>
2023-01-04 13:59:30 +0000

    added ' - ' to consoles

--- console.js
+++ console.js
@@ -1,14 +1,14 @@
 //Set of console functions to be used in projects
 //Created by: JWL
 //Date: 2022-12-30
-//Last modified: 2023/01/03 15:35:32
+//Last modified: 2023/01/04 13:59:16
 //Version: 0.0.1
 
 //log out a message to the console with a timestamp always
 function log(message) {
   var date = new Date();
   var time = date.toLocaleTimeString();
-  console.log(time + " " + message);
+  console.log(time + " - " + message);
 }
 
 //log out a message to the console with a timestamp only if the debug flag is set
@@ -16,7 +16,7 @@ function logDebug(message) {
   if (global.debugFlag) {
     var date = new Date();
     var time = date.toLocaleTimeString();
-    console.log(time + " " + message);
+    console.log(time + " - " + message);
   }
 }
 
